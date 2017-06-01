f6fcafb456c20df094e8dbf2a57e7e3d73f1c2ba
shumkj <seansam06@gmail.com>
2017-06-01 14:07:29 +1000

    create main page

--- src/asgn2Wizards/PizzaWizard.java
+++ src/asgn2Wizards/PizzaWizard.java
@@ -6,7 +6,7 @@ import javax.swing.SwingUtilities;
 import asgn2GUIs.PizzaGUI;
 
 /**
- * This class is the ‘entry point’ to the rest of the system and provides a public static void main method. 
+ * This class is the entry point to the rest of the system and provides a public static void main method. 
  * At the moment, this just calls the asgn2GUIs.PizzaGUI class. You can probably leave the class as it is,
  *  however, you must make sure that it is the one and only entry point to the rest of the system. 
  *  
@@ -26,7 +26,7 @@ public class PizzaWizard {
 	 * @param args Command Line Arguments
 	 */
 	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new PizzaGUI("Pizza Place"));		
+		SwingUtilities.invokeLater(new PizzaGUI("Pizza Place"));	
 		
 	}
 

8d3ebbdc5c48ff13f335a98ae28371640ce14e29
shumkj <seansam06@gmail.com>
2017-06-02 20:43:17 +1000

    Reformatting

--- src/asgn2Tests/CustomerTests.java
+++ src/asgn2Tests/CustomerTests.java
@@ -1,4 +1,5 @@
 package asgn2Tests;
+
 import static org.junit.Assert.*;
 
 import org.junit.Before;
@@ -6,156 +7,152 @@ import org.junit.Test;
 
 import asgn2Exceptions.CustomerException;
 import asgn2Customers.Customer;
-import asgn2Customers.PickUpCustomer; 
-import asgn2Customers.DriverDeliveryCustomer; 
-import asgn2Customers.DroneDeliveryCustomer; 
-
-
-
+import asgn2Customers.PickUpCustomer;
+import asgn2Customers.DriverDeliveryCustomer;
+import asgn2Customers.DroneDeliveryCustomer;
 
 /**
- * A class that tests the that tests the asgn2Customers.PickUpCustomer, asgn2Customers.DriverDeliveryCustomer,
- * asgn2Customers.DroneDeliveryCustomer classes. Note that an instance of asgn2Customers.DriverDeliveryCustomer 
- * should be used to test the functionality of the  asgn2Customers.Customer abstract class. 
+ * A class that tests the that tests the asgn2Customers.PickUpCustomer,
+ * asgn2Customers.DriverDeliveryCustomer, asgn2Customers.DroneDeliveryCustomer
+ * classes. Note that an instance of asgn2Customers.DriverDeliveryCustomer
+ * should be used to test the functionality of the asgn2Customers.Customer
+ * abstract class.
  * 
- * @author Person A
+ * @author Veng Sie Choy
  * 
  *
  */
 public class CustomerTests {
-	Customer DVC; 
-	Customer DNC; 
-	Customer PUC; 
-	
-	@Before 
+	Customer DVC;
+	Customer DNC;
+	Customer PUC;
+
+	@Before
 	public void Initialize() throws CustomerException {
-		DVC = new DriverDeliveryCustomer ("Sean", "0402024300", 4, 3);
-		DNC = new DroneDeliveryCustomer ("Gary", "0472774229", 4, 3);
-		PUC = new PickUpCustomer ("Kelok", "0423241145", 0, 0);
-		
-	}
-	
-	@Test (expected = CustomerException.class)
-	public void testConstructorNameMoreThan20Digits() throws CustomerException {
-		DVC = new DriverDeliveryCustomer ("Uvuvwevwevwe Onyetenyevwe Ossas", "0214252252", 4, 3); 
-		
-	}
-	
-	@Test (expected = CustomerException.class)
-	public void testConstructorNameForBlankSpace() throws CustomerException {
-		DVC = new DriverDeliveryCustomer (" ", "0898989898", 4, 3);
-		
-	}
-	
-	@Test (expected = CustomerException.class)
-	public void testConstructorNameForEmptyString() throws CustomerException {
-		DVC = new DriverDeliveryCustomer ("", "0898989898", 4, 3);
-		
-	}
-	
-	@Test (expected = CustomerException.class)
-	public void testConstructorMobileNumberStartWithDigit1() throws CustomerException {
-		DVC = new DriverDeliveryCustomer ("Sean", "1234567890", 4, 3);
-		
-	}
-		
-	@Test (expected = CustomerException.class)
-	public void testConstructorMobileNumberLessThan10Digits() throws CustomerException {
-		DVC = new DriverDeliveryCustomer ("Sean", "023456789", 4, 3);
-			
-	}
-	
-	@Test (expected = CustomerException.class)
-	public void testConstructorMobileNumberMoreThan10Digits() throws CustomerException {
-		DVC = new DriverDeliveryCustomer ("Sean", "02345678910", 4, 3);
-			
-	}
-	
-	@Test (expected = CustomerException.class)
-	public void testConstructorMobileNumberWithSymbol() throws CustomerException {
-		DVC = new DriverDeliveryCustomer ("Sean", "0563832@4^", 4, 3);
-			
-	}
-	
-	
-	@Test (expected = CustomerException.class) 
-	public void testConstructorForLocationXMoreThan10() throws CustomerException {
-		DVC = new DriverDeliveryCustomer ("Sean", "0898989898", 11, 3);
-		
-	}
-	
-	@Test (expected = CustomerException.class) 
-	public void testConstructorForLocationYMoreThan10() throws CustomerException {
-		DVC = new DriverDeliveryCustomer ("Sean", "0898989898", 4, 11);
-		
-	}
-	
-	@Test 
-	public void testLocationXForPickUpCustomerShouldBeZero() throws CustomerException {
-		PUC = new PickUpCustomer("Kelok", "0423241145", 2, 0); 
-		assertEquals(0, PUC.getLocationX());
-		
-	}
-	
-	@Test 
-	public void testLocationYForPickUpCustomerShouldBeZero() throws CustomerException {
-		PUC = new PickUpCustomer("Kelok", "0423241145", 0, 2); 
-		assertEquals(0, PUC.getLocationY());
-		
-	}
-	
-	@Test 
-	public void testGetMobileNumber() throws CustomerException {
-		assertEquals("0402024300", DVC.getMobileNumber()); 
-		
-	}
-	
-	@Test 
-	public void testGetCustomerType() throws CustomerException {
-		assertEquals("Pick Up", PUC.getCustomerType()); 
-		assertEquals("Driver Delivery", DVC.getCustomerType()); 
-		assertEquals("Drone Delivery", DNC.getCustomerType()); 
-		
-	}
-	
-	@Test 
-	public void testGetLocationX() throws CustomerException {
-		assertEquals(4, DVC.getLocationX()); 
-		
-	}
-	
-	@Test 
-	public void testGetLocationY() throws CustomerException {
-		assertEquals(3, DVC.getLocationY()); 
-		
-	}
-		
-	@Test 
-	public void testGetCustomerNameForDifferentTypeOfCustomer() throws CustomerException {
-		assertEquals("Sean", DVC.getName()); 
-		assertEquals("Gary", DNC.getName()); 
-		assertEquals("Kelok", PUC.getName()); 
-		
+		DVC = new DriverDeliveryCustomer("Sean", "0402024300", 4, 3);
+		DNC = new DroneDeliveryCustomer("Gary", "0472774229", 4, 3);
+		PUC = new PickUpCustomer("Kelok", "0423241145", 0, 0);
+
 	}
 
-	@Test 
+	@Test(expected = CustomerException.class)
+	public void testConstructorNameMoreThan20Digits() throws CustomerException {
+		DVC = new DriverDeliveryCustomer("Uvuvwevwevwe Onyetenyevwe Ossas", "0214252252", 4, 3);
+
+	}
+
+	@Test(expected = CustomerException.class)
+	public void testConstructorNameForBlankSpace() throws CustomerException {
+		DVC = new DriverDeliveryCustomer(" ", "0898989898", 4, 3);
+
+	}
+
+	@Test(expected = CustomerException.class)
+	public void testConstructorNameForEmptyString() throws CustomerException {
+		DVC = new DriverDeliveryCustomer("", "0898989898", 4, 3);
+
+	}
+
+	@Test(expected = CustomerException.class)
+	public void testConstructorMobileNumberStartWithDigit1() throws CustomerException {
+		DVC = new DriverDeliveryCustomer("Sean", "1234567890", 4, 3);
+
+	}
+
+	@Test(expected = CustomerException.class)
+	public void testConstructorMobileNumberLessThan10Digits() throws CustomerException {
+		DVC = new DriverDeliveryCustomer("Sean", "023456789", 4, 3);
+
+	}
+
+	@Test(expected = CustomerException.class)
+	public void testConstructorMobileNumberMoreThan10Digits() throws CustomerException {
+		DVC = new DriverDeliveryCustomer("Sean", "02345678910", 4, 3);
+
+	}
+
+	@Test(expected = CustomerException.class)
+	public void testConstructorMobileNumberWithSymbol() throws CustomerException {
+		DVC = new DriverDeliveryCustomer("Sean", "0563832@4^", 4, 3);
+
+	}
+
+	@Test(expected = CustomerException.class)
+	public void testConstructorForLocationXMoreThan10() throws CustomerException {
+		DVC = new DriverDeliveryCustomer("Sean", "0898989898", 11, 3);
+
+	}
+
+	@Test(expected = CustomerException.class)
+	public void testConstructorForLocationYMoreThan10() throws CustomerException {
+		DVC = new DriverDeliveryCustomer("Sean", "0898989898", 4, 11);
+
+	}
+
+	@Test
+	public void testLocationXForPickUpCustomerShouldBeZero() throws CustomerException {
+		PUC = new PickUpCustomer("Kelok", "0423241145", 2, 0);
+		assertEquals(0, PUC.getLocationX());
+
+	}
+
+	@Test
+	public void testLocationYForPickUpCustomerShouldBeZero() throws CustomerException {
+		PUC = new PickUpCustomer("Kelok", "0423241145", 0, 2);
+		assertEquals(0, PUC.getLocationY());
+
+	}
+
+	@Test
+	public void testGetMobileNumber() throws CustomerException {
+		assertEquals("0402024300", DVC.getMobileNumber());
+
+	}
+
+	@Test
+	public void testGetCustomerType() throws CustomerException {
+		assertEquals("Pick Up", PUC.getCustomerType());
+		assertEquals("Driver Delivery", DVC.getCustomerType());
+		assertEquals("Drone Delivery", DNC.getCustomerType());
+
+	}
+
+	@Test
+	public void testGetLocationX() throws CustomerException {
+		assertEquals(4, DVC.getLocationX());
+
+	}
+
+	@Test
+	public void testGetLocationY() throws CustomerException {
+		assertEquals(3, DVC.getLocationY());
+
+	}
+
+	@Test
+	public void testGetCustomerNameForDifferentTypeOfCustomer() throws CustomerException {
+		assertEquals("Sean", DVC.getName());
+		assertEquals("Gary", DNC.getName());
+		assertEquals("Kelok", PUC.getName());
+
+	}
+
+	@Test
 	public void testGetDeliveryDistanceForDroneDeliveryCustomer() throws CustomerException {
-		assertEquals(5.0, DNC.getDeliveryDistance(), 0.0); 
-		
+		assertEquals(5.0, DNC.getDeliveryDistance(), 0.0);
+
 	}
-	
-	@Test 
+
+	@Test
 	public void testGetDeliveryDistanceForDriverDeliveryCustomer() throws CustomerException {
-		assertEquals(7.0, DVC.getDeliveryDistance(), 0.0); 
-		
+		assertEquals(7.0, DVC.getDeliveryDistance(), 0.0);
+
 	}
-	
+
 	@Test
 	public void testGetDeliveryDistanceForPickUpCustomer() throws CustomerException {
 		assertEquals(0, PUC.getDeliveryDistance(), 0);
-		
+
 	}
 
-	
 }
-
--- src/asgn2Tests/LogHandlerPizzaTests.java
+++ src/asgn2Tests/LogHandlerPizzaTests.java
@@ -8,11 +8,13 @@ import asgn2Exceptions.LogHandlerException;
 import asgn2Exceptions.PizzaException;
 import asgn2Restaurant.LogHandler;
 
-/** A class that tests the methods relating to the creation of Pizza objects in the asgn2Restaurant.LogHander class.
-* 
-* @author Person B
-* 
-*/
+/**
+ * A class that tests the methods relating to the creation of Pizza objects in
+ * the asgn2Restaurant.LogHander class.
+ * 
+ * @author Khar Jun Shum
+ * 
+ */
 public class LogHandlerPizzaTests {
 	final static String FILE_1 = Paths.get("logs/20170101.txt").toString();
 	final static String FILE_2 = Paths.get("logs/20170102.txt").toString();
@@ -27,142 +29,153 @@ public class LogHandlerPizzaTests {
 	final static String FILE_11 = Paths.get("logs/unit-test-logs/pizza/invalid-format-order-time.txt").toString();
 	final static String FILE_12 = Paths.get("logs/unit-test-logs/pizza/negative-order-time.txt").toString();
 	final static String FILE_13 = Paths.get("logs/unit-test-logs/pizza/exceed-hour-limit-delivery-time.txt").toString();
-	final static String FILE_14 = Paths.get("logs/unit-test-logs/pizza/exceed-minute-limit-delivery-time.txt").toString();
+	final static String FILE_14 = Paths.get("logs/unit-test-logs/pizza/exceed-minute-limit-delivery-time.txt")
+			.toString();
 	final static String FILE_15 = Paths.get("logs/unit-test-logs/pizza/invalid-format-delivery-time.txt").toString();
 	final static String FILE_16 = Paths.get("logs/unit-test-logs/pizza/negative-delivery-time.txt").toString();
-	
-	@Test (expected = LogHandlerException.class)
+
+	@Test(expected = LogHandlerException.class)
 	public void testNoFileGivenWillThrowException() throws PizzaException, LogHandlerException {
 		LogHandler.populatePizzaDataset("");
 	}
-	
-	@Test (expected = LogHandlerException.class)
+
+	@Test(expected = LogHandlerException.class)
 	public void testFileNotFoundWillThrowException() throws PizzaException, LogHandlerException {
 		LogHandler.populatePizzaDataset("unknown-file.txt");
 	}
-	
-	@Test (expected = LogHandlerException.class)
-	public void testWrongFileFormatWillThrowException() throws PizzaException, LogHandlerException{
+
+	@Test(expected = LogHandlerException.class)
+	public void testWrongFileFormatWillThrowException() throws PizzaException, LogHandlerException {
 		LogHandler.populatePizzaDataset(FILE_4);
 	}
-	
-	@Test (expected = LogHandlerException.class)
-	public void testWrongQuantityFormatWillThrowException() throws PizzaException, LogHandlerException{
+
+	@Test(expected = LogHandlerException.class)
+	public void testWrongQuantityFormatWillThrowException() throws PizzaException, LogHandlerException {
 		LogHandler.populatePizzaDataset(FILE_5);
 	}
-	
-	@Test (expected = LogHandlerException.class)
-	public void testQuantityLimitOverflowPositiveValueWillThrowException() throws PizzaException, LogHandlerException{
+
+	@Test(expected = LogHandlerException.class)
+	public void testQuantityLimitOverflowPositiveValueWillThrowException() throws PizzaException, LogHandlerException {
 		LogHandler.populatePizzaDataset(FILE_6);
 	}
-	
-	@Test (expected = LogHandlerException.class)
-	public void testQuantityLimitOverflowNegativeValueWillThrowException() throws PizzaException, LogHandlerException{
+
+	@Test(expected = LogHandlerException.class)
+	public void testQuantityLimitOverflowNegativeValueWillThrowException() throws PizzaException, LogHandlerException {
 		LogHandler.populatePizzaDataset(FILE_7);
 	}
-	
-	@Test (expected = LogHandlerException.class)
-	public void testDoubleFormatQuantityWillThrowException() throws PizzaException, LogHandlerException{
+
+	@Test(expected = LogHandlerException.class)
+	public void testDoubleFormatQuantityWillThrowException() throws PizzaException, LogHandlerException {
 		LogHandler.populatePizzaDataset(FILE_8);
 	}
-	
-	@Test (expected = LogHandlerException.class)
-	public void testExceedsHourLimitAtOrderTimeWillThrowException() throws PizzaException, LogHandlerException{
+
+	@Test(expected = LogHandlerException.class)
+	public void testExceedsHourLimitAtOrderTimeWillThrowException() throws PizzaException, LogHandlerException {
 		LogHandler.populatePizzaDataset(FILE_9);
 	}
-	
-	@Test (expected = LogHandlerException.class)
-	public void testExceedsMinuteLimitAtOrderTimeWillThrowException() throws PizzaException, LogHandlerException{
+
+	@Test(expected = LogHandlerException.class)
+	public void testExceedsMinuteLimitAtOrderTimeWillThrowException() throws PizzaException, LogHandlerException {
 		LogHandler.populatePizzaDataset(FILE_10);
 	}
-	
-	@Test (expected = LogHandlerException.class)
-	public void testInvalidFormatAtOrderTimeWillThrowException() throws PizzaException, LogHandlerException{
+
+	@Test(expected = LogHandlerException.class)
+	public void testInvalidFormatAtOrderTimeWillThrowException() throws PizzaException, LogHandlerException {
 		LogHandler.populatePizzaDataset(FILE_11);
 	}
-	
-	@Test (expected = LogHandlerException.class)
-	public void testNegativeValueForOrderTimeWillThrowException() throws PizzaException, LogHandlerException{
+
+	@Test(expected = LogHandlerException.class)
+	public void testNegativeValueForOrderTimeWillThrowException() throws PizzaException, LogHandlerException {
 		LogHandler.populatePizzaDataset(FILE_12);
 	}
-	
-	@Test (expected = LogHandlerException.class)
-	public void testExceedsHourLimitAtDeliveryTimeWillThrowException() throws PizzaException, LogHandlerException{
+
+	@Test(expected = LogHandlerException.class)
+	public void testExceedsHourLimitAtDeliveryTimeWillThrowException() throws PizzaException, LogHandlerException {
 		LogHandler.populatePizzaDataset(FILE_13);
 	}
-	
-	@Test (expected = LogHandlerException.class)
-	public void testExceedsMinuteLimitAtDeliveryTimeWillThrowException() throws PizzaException, LogHandlerException{
+
+	@Test(expected = LogHandlerException.class)
+	public void testExceedsMinuteLimitAtDeliveryTimeWillThrowException() throws PizzaException, LogHandlerException {
 		LogHandler.populatePizzaDataset(FILE_14);
 	}
-	
-	@Test (expected = LogHandlerException.class)
-	public void testInvalidFormatAtDeliveryTimeWillThrowException() throws PizzaException, LogHandlerException{
+
+	@Test(expected = LogHandlerException.class)
+	public void testInvalidFormatAtDeliveryTimeWillThrowException() throws PizzaException, LogHandlerException {
 		LogHandler.populatePizzaDataset(FILE_15);
 	}
-	
-	@Test (expected = LogHandlerException.class)
-	public void testNegativeValueForDeliveryTimeWillThrowException() throws PizzaException, LogHandlerException{
+
+	@Test(expected = LogHandlerException.class)
+	public void testNegativeValueForDeliveryTimeWillThrowException() throws PizzaException, LogHandlerException {
 		LogHandler.populatePizzaDataset(FILE_16);
 	}
-	
-	@Test (expected = LogHandlerException.class)
-	public void testWrongQuantityFormatWillThrowExceptionForCreatePizza() throws PizzaException, LogHandlerException{
+
+	@Test(expected = LogHandlerException.class)
+	public void testWrongQuantityFormatWillThrowExceptionForCreatePizza() throws PizzaException, LogHandlerException {
 		LogHandler.createPizza("19:00:00,19:20:00,Casey Jones,0123456789,DVC,5,5,PZV,a");
 	}
-	
-	@Test (expected = LogHandlerException.class)
-	public void testQuantityLimitOverflowPositiveValueWillThrowExceptionForCreatePizza() throws PizzaException, LogHandlerException{
+
+	@Test(expected = LogHandlerException.class)
+	public void testQuantityLimitOverflowPositiveValueWillThrowExceptionForCreatePizza()
+			throws PizzaException, LogHandlerException {
 		LogHandler.createPizza("19:00:00,19:20:00,Casey Jones,0123456789,DVC,5,5,PZV,2147483648");
 	}
-	
-	@Test (expected = LogHandlerException.class)
-	public void testQuantityLimitOverflowNegativeValueWillThrowExceptionForCreatePizza() throws PizzaException, LogHandlerException{
+
+	@Test(expected = LogHandlerException.class)
+	public void testQuantityLimitOverflowNegativeValueWillThrowExceptionForCreatePizza()
+			throws PizzaException, LogHandlerException {
 		LogHandler.createPizza("19:00:00,19:20:00,Casey Jones,0123456789,DVC,5,5,PZV,-2147483649");
 	}
-	
-	@Test (expected = LogHandlerException.class)
-	public void testDoubleFormatQuantityWillThrowExceptionForCreatePizza() throws PizzaException, LogHandlerException{
+
+	@Test(expected = LogHandlerException.class)
+	public void testDoubleFormatQuantityWillThrowExceptionForCreatePizza() throws PizzaException, LogHandlerException {
 		LogHandler.createPizza("19:00:00,19:20:00,Casey Jones,0123456789,DVC,5,5,PZV,2.0");
 	}
-	
-	@Test (expected = LogHandlerException.class)
-	public void testExceedsHourLimitAtOrderTimeWillThrowExceptionForCreatePizza() throws PizzaException, LogHandlerException{
+
+	@Test(expected = LogHandlerException.class)
+	public void testExceedsHourLimitAtOrderTimeWillThrowExceptionForCreatePizza()
+			throws PizzaException, LogHandlerException {
 		LogHandler.createPizza("25:00:00,19:20:00,Casey Jones,0123456789,DVC,5,5,PZV,2");
 	}
-	
-	@Test (expected = LogHandlerException.class)
-	public void testExceedsMinuteLimitAtOrderTimeWillThrowExceptionForCreatePizza() throws PizzaException, LogHandlerException{
+
+	@Test(expected = LogHandlerException.class)
+	public void testExceedsMinuteLimitAtOrderTimeWillThrowExceptionForCreatePizza()
+			throws PizzaException, LogHandlerException {
 		LogHandler.createPizza("19:70:00,19:20:00,Casey Jones,0123456789,DVC,5,5,PZV,2");
 	}
-	
-	@Test (expected = LogHandlerException.class)
-	public void testInvalidFormatAtOrderTimeWillThrowExceptionForCreatePizza() throws PizzaException, LogHandlerException{
+
+	@Test(expected = LogHandlerException.class)
+	public void testInvalidFormatAtOrderTimeWillThrowExceptionForCreatePizza()
+			throws PizzaException, LogHandlerException {
 		LogHandler.createPizza("ikuzo,19:20:00,Casey Jones,0123456789,DVC,5,5,PZV,2");
 	}
-	
-	@Test (expected = LogHandlerException.class)
-	public void testNegativeValueForOrderTimeWillThrowExceptionForCreatePizza() throws PizzaException, LogHandlerException{
+
+	@Test(expected = LogHandlerException.class)
+	public void testNegativeValueForOrderTimeWillThrowExceptionForCreatePizza()
+			throws PizzaException, LogHandlerException {
 		LogHandler.createPizza("-19:00:00,19:20:00,Casey Jones,0123456789,DVC,5,5,PZV,2");
 	}
-	
-	@Test (expected = LogHandlerException.class)
-	public void testExceedsHourLimitAtDeliveryTimeWillThrowExceptionForCreatePizza() throws PizzaException, LogHandlerException{
+
+	@Test(expected = LogHandlerException.class)
+	public void testExceedsHourLimitAtDeliveryTimeWillThrowExceptionForCreatePizza()
+			throws PizzaException, LogHandlerException {
 		LogHandler.createPizza("19:00:00,25:20:00,Casey Jones,0123456789,DVC,5,5,PZV,2");
 	}
-	
-	@Test (expected = LogHandlerException.class)
-	public void testExceedsMinuteLimitAtDeliveryTimeWillThrowExceptionForCreatePizza() throws PizzaException, LogHandlerException{
+
+	@Test(expected = LogHandlerException.class)
+	public void testExceedsMinuteLimitAtDeliveryTimeWillThrowExceptionForCreatePizza()
+			throws PizzaException, LogHandlerException {
 		LogHandler.createPizza("19:00:00,19:80:00,Casey Jones,0123456789,DVC,5,5,PZV,2");
 	}
-	
-	@Test (expected = LogHandlerException.class)
-	public void testInvalidFormatAtDeliveryTimeWillThrowExceptionForCreatePizza() throws PizzaException, LogHandlerException{
+
+	@Test(expected = LogHandlerException.class)
+	public void testInvalidFormatAtDeliveryTimeWillThrowExceptionForCreatePizza()
+			throws PizzaException, LogHandlerException {
 		LogHandler.createPizza("19:00:00,ikuzo,Casey Jones,0123456789,DVC,5,5,PZV,2");
 	}
-	
-	@Test (expected = LogHandlerException.class)
-	public void testNegativeValueForDeliveryTimeWillThrowExceptionForCreatePizza() throws PizzaException, LogHandlerException{
+
+	@Test(expected = LogHandlerException.class)
+	public void testNegativeValueForDeliveryTimeWillThrowExceptionForCreatePizza()
+			throws PizzaException, LogHandlerException {
 		LogHandler.createPizza("19:00:00,-19:20:00,Casey Jones,0123456789,DVC,5,5,PZV,2");
 	}
 }

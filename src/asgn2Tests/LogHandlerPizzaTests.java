8738fd27146620e82e849aa8962241af46820fda
shumkj <seansam06@gmail.com>
2017-06-02 16:45:11 +1000

    Added tests files, tests case

--- src/asgn2Tests/LogHandlerPizzaTests.java
+++ src/asgn2Tests/LogHandlerPizzaTests.java
@@ -1,5 +1,12 @@
 package asgn2Tests;
 
+import java.nio.file.Paths;
+
+import org.junit.Test;
+
+import asgn2Exceptions.LogHandlerException;
+import asgn2Exceptions.PizzaException;
+import asgn2Restaurant.LogHandler;
 
 /** A class that tests the methods relating to the creation of Pizza objects in the asgn2Restaurant.LogHander class.
 * 
@@ -7,5 +14,107 @@ package asgn2Tests;
 * 
 */
 public class LogHandlerPizzaTests {
+	final static String FILE_1 = Paths.get("logs/20170101.txt").toString();
+	final static String FILE_2 = Paths.get("logs/20170102.txt").toString();
+	final static String FILE_3 = Paths.get("logs/20170103.txt").toString();
+	final static String FILE_4 = Paths.get("logs/unit-test-logs/pizza/wrong-file-format.png").toString();
+	final static String FILE_5 = Paths.get("logs/unit-test-logs/pizza/wrong-quantity-format.txt").toString();
+	final static String FILE_6 = Paths.get("logs/unit-test-logs/pizza/quantity-overflow-positive.txt").toString();
+	final static String FILE_7 = Paths.get("logs/unit-test-logs/pizza/quantity-overflow-negative.txt").toString();
+	final static String FILE_8 = Paths.get("logs/unit-test-logs/pizza/double-format-quantity.txt").toString();
+	final static String FILE_9 = Paths.get("logs/unit-test-logs/pizza/exceed-hour-limit-order-time.txt").toString();
+	final static String FILE_10 = Paths.get("logs/unit-test-logs/pizza/exceed-minute-limit-order-time.txt").toString();
+	final static String FILE_11 = Paths.get("logs/unit-test-logs/pizza/invalid-format-order-time.txt").toString();
+	final static String FILE_12 = Paths.get("logs/unit-test-logs/pizza/double-format-order-time.txt").toString();	
+	final static String FILE_13 = Paths.get("logs/unit-test-logs/pizza/negative-order-time.txt").toString();
+	final static String FILE_14 = Paths.get("logs/unit-test-logs/pizza/exceed-hour-limit-delivery-time.txt").toString();
+	final static String FILE_15 = Paths.get("logs/unit-test-logs/pizza/exceed-minute-limit-delivery-time.txt").toString();
+	final static String FILE_16 = Paths.get("logs/unit-test-logs/pizza/invalid-format-delivery-time.txt").toString();
+	final static String FILE_17 = Paths.get("logs/unit-test-logs/pizza/double-format-delivery-time.txt").toString();
+	final static String FILE_18 = Paths.get("logs/unit-test-logs/pizza/negative-delivery-time.txt").toString();
 	
+	@Test (expected = LogHandlerException.class)
+	public void testNoFileGivenWillThrowException() throws PizzaException, LogHandlerException {
+		LogHandler.populatePizzaDataset("");
+	}
+	
+	@Test (expected = LogHandlerException.class)
+	public void testFileNotFoundWillThrowException() throws PizzaException, LogHandlerException {
+		LogHandler.populatePizzaDataset("unknown-file.txt");
+	}
+	
+	@Test (expected = LogHandlerException.class)
+	public void testWrongFileFormatWillThrowException() throws PizzaException, LogHandlerException{
+		LogHandler.populatePizzaDataset(FILE_4);
+	}
+	
+	@Test (expected = LogHandlerException.class)
+	public void testWrongQuantityFormatWillThrowException() throws PizzaException, LogHandlerException{
+		LogHandler.populatePizzaDataset(FILE_5);
+	}
+	
+	@Test (expected = LogHandlerException.class)
+	public void testQuantityLimitOverflowPositiveValueWillThrowException() throws PizzaException, LogHandlerException{
+		LogHandler.populatePizzaDataset(FILE_6);
+	}
+	
+	@Test (expected = LogHandlerException.class)
+	public void testQuantityLimitOverflowNegativeValueWillThrowException() throws PizzaException, LogHandlerException{
+		LogHandler.populatePizzaDataset(FILE_7);
+	}
+	
+	@Test (expected = LogHandlerException.class)
+	public void testDoubleFormatQuantityWillThrowException() throws PizzaException, LogHandlerException{
+		LogHandler.populatePizzaDataset(FILE_8);
+	}
+	
+	@Test (expected = LogHandlerException.class)
+	public void testExceedsHourLimitAtOrderTimeWillThrowException() throws PizzaException, LogHandlerException{
+		LogHandler.populatePizzaDataset(FILE_9);
+	}
+	
+	@Test (expected = LogHandlerException.class)
+	public void testExceedsMinuteLimitAtOrderTimeWillThrowException() throws PizzaException, LogHandlerException{
+		LogHandler.populatePizzaDataset(FILE_10);
+	}
+	
+	@Test (expected = LogHandlerException.class)
+	public void testInvalidFormatAtOrderTimeWillThrowException() throws PizzaException, LogHandlerException{
+		LogHandler.populatePizzaDataset(FILE_11);
+	}
+	
+	@Test (expected = LogHandlerException.class)
+	public void testDoubleFormatAtOrderTimeWillThrowException() throws PizzaException, LogHandlerException{
+		LogHandler.populatePizzaDataset(FILE_12);
+	}
+	
+	@Test (expected = LogHandlerException.class)
+	public void testNegativeValueForOrderTimeWillThrowException() throws PizzaException, LogHandlerException{
+		LogHandler.populatePizzaDataset(FILE_13);
+	}
+	
+	@Test (expected = LogHandlerException.class)
+	public void testExceedsHourLimitAtDeliveryTimeWillThrowException() throws PizzaException, LogHandlerException{
+		LogHandler.populatePizzaDataset(FILE_14);
+	}
+	
+	@Test (expected = LogHandlerException.class)
+	public void testExceedsMinuteLimitAtDeliveryTimeWillThrowException() throws PizzaException, LogHandlerException{
+		LogHandler.populatePizzaDataset(FILE_15);
+	}
+	
+	@Test (expected = LogHandlerException.class)
+	public void testInvalidFormatAtDeliveryTimeWillThrowException() throws PizzaException, LogHandlerException{
+		LogHandler.populatePizzaDataset(FILE_16);
+	}
+	
+	@Test (expected = LogHandlerException.class)
+	public void testDoubleFormatAtDeliveryTimeWillThrowException() throws PizzaException, LogHandlerException{
+		LogHandler.populatePizzaDataset(FILE_17);
+	}
+	
+	@Test (expected = LogHandlerException.class)
+	public void testNegativeValueForDeliveryTimeWillThrowException() throws PizzaException, LogHandlerException{
+		LogHandler.populatePizzaDataset(FILE_18);
+	}
 }
